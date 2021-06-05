8cd5dcda794aeeef370189e10b814ebf0f1c92ef
Eze <81213101@qq.com>
2021-06-05 11:21:18 +0800

    release project

--- coco.config.js
+++ coco.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'coco-template',
   templateName: '营销活动',
-  author: 'muwoo',
-  snapshot: 'https://cdn.img.snapshot.png',
-  gitUrl: 'coco-h5/coco-template',
+  author: 'eze',
+  snapshot: 'https://krpano.deyou360.com/110/cantin.tiles/pano_f.jpg',
+  gitUrl: '720lego/coco-template',
   projectType: '0'
 };
